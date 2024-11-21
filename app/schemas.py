8935f5435db3fe9bd2bd347e82154bb00cb351fa
Fernandosoriano <fersoriano@ciencias.unam.mx>
2024-11-20 23:53:12 -0600

    schema modified

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
-
-class SentimentCreate(BaseModel):
-    text: str
-
 class Sentiment(BaseModel):
     id: int
     text: str
     sentiment: str
     created_at: datetime
-
-    class Config:
-        orm_mode = True
